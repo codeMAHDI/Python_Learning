d2eed0b822501a2b82ae26390456d906c9961b78
Mahdi <moshiurrahmanmehedi210@gmail.com>
2026-04-04 19:52:30 +0600

    Updated

--- All_python_code/hello.py
+++ All_python_code/hello.py
@@ -6,3 +6,16 @@ first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name.title()} \n {last_name.title()}"
 print(f"Hello, {full_name.title()}!")
+
+print("Languages:\nPython\nC\nJavaScript")
+
+# Stripping Whitespace using rstrip(), lstrip(), and strip() methods
+favorite_language = 'python '
+print(favorite_language)
+print(favorite_language.rstrip())
+favorite_language = ' python '
+print(favorite_language)
+print(favorite_language.lstrip())
+favorite_language = ' python '
+print(favorite_language)   
+print(favorite_language.strip())
