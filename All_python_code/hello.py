c8e757cbdc59bd30bb5d6aac3596f157b05d0373
Mahdi <moshiurrahmanmehedi210@gmail.com>
2026-04-04 19:45:34 +0600

    first commit

---
+++ All_python_code/hello.py
@@ -0,0 +1,8 @@
+name = "Ada Lovelace Is A Bad Women"
+print(name.upper())
+print(name.lower())
+print(name.title())
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name.title()} \n {last_name.title()}"
+print(f"Hello, {full_name.title()}!")
