eba68ab9994092abb0200701154745efe1f9b22d
Mahdi <moshiurrahmanmehedi210@gmail.com>
2026-04-19 12:30:53 +0600

    Modifying, Adding, and Removing Elements

--- All_python_code/list.py
+++ All_python_code/list.py
@@ -3,5 +3,15 @@ digits =[1,"tarek",3,4,5 ]
 print(digits)
 print(digits[2])
 print(digits[1].title())
+
 # Access last element
 print(digits[-1])
+
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message= f"My first bicycle was a {bicycles[2]}"
+print(message)
+
+
+# Modifying, Adding, and Removing Elements
+
