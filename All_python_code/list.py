0c70d9683ebcb398508eb7e1120b398f31f65484
Mahdi <moshiurrahmanmehedi210@gmail.com>
2026-04-19 11:19:12 +0600

    Changes

---
+++ All_python_code/list.py
@@ -0,0 +1,7 @@
+# List
+digits =[1,"tarek",3,4,5 ]
+print(digits)
+print(digits[2])
+print(digits[1].title())
+# Access last element
+print(digits[-1])
--- My_programming_language/main.py
+++ My_programming_language/main.py
@@ -1,8 +1,37 @@
-code= input("Bangla Code Likho:")
+# Bangla Language - Step 2 (Variables + Print)
 
-if code.startswith("DEKH VAI"):
-    text= code.replace("DEKH VAI","").strip()
-    print(text)
-else:
-    print("Unknown command")
-    
+memory = {}   # ekhane variables store hobe
+
+while True:
+    code = input("Bangla Code Likho: ")
+
+    # exit system (optional)
+    if code == "EXIT":
+        break
+
+    # =========================
+    # 1. PRINT SYSTEM
+    # =========================
+    if code.startswith("DEKH VAI"):
+        text = code.replace("DEKH VAI", "").strip()
+
+        # variable check
+        if text in memory:
+            print(memory[text])
+        else:
+            print(text)
+
+    # =========================
+    # 2. VARIABLE SYSTEM
+    # =========================
+    elif code.startswith("NIRDHARON"):
+        parts = code.replace("NIRDHARON", "").strip().split("=")
+
+        var_name = parts[0].strip()
+        value = parts[1].strip()
+
+        memory[var_name] = value
+        print(f"{var_name} saved!")
+
+    else:
+        print("Unknown command")
